a5d90b24b332699cd67e0d0b95254ec7a0e4d285
matiiv <matiiv.r.a@gmail.com>
2021-06-06 20:36:23 +0300

    write test

---
+++ lesson_1/A/test_heat.py
@@ -0,0 +1,82 @@
+from solution import heat_mode
+
+
+def test_heat_mode_equal_1():
+    t_room = -10
+    t_cond = -10
+
+    want_temp = t_room
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_equal_2():
+    t_room = 0
+    t_cond = 0
+
+    want_temp = t_room
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_equal_3():
+    t_room = 10
+    t_cond = 10
+
+    want_temp = t_room
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_above_1():
+    t_room = -10
+    t_cond = 0
+
+    want_temp = t_cond
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_above_2():
+    t_room = -10
+    t_cond = 10
+
+    want_temp = t_cond
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_above_3():
+    t_room = 0
+    t_cond = 10
+
+    want_temp = t_cond
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_less_1():
+    t_room = 0
+    t_cond = -10
+
+    want_temp = t_room
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_less_2():
+    t_room = 10
+    t_cond = -10
+
+    want_temp = t_room
+
+    assert heat_mode(t_room, t_cond) == want_temp
+
+
+def test_heat_mode_less_3():
+    t_room = 20
+    t_cond = 10
+
+    want_temp = t_room
+
+    assert heat_mode(t_room, t_cond) == want_temp
