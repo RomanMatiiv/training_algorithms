aa5bff2ee5bb1ff5f30ab047ccdb14f372e70b46
matiiv <matiiv.r.a@gmail.com>
2021-06-07 07:27:45 +0300

    write test for solution b

---
+++ lesson_1/B/solution_b.py
@@ -0,0 +1,22 @@
+
+
+def triangle_exist(a: int, b: int, c: int) -> bool:
+    if (a + b) <= c:
+        return False
+    if (a + c) <= b:
+        return False
+    if (b + c) <= a:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    c = int(input())
+
+    if triangle_exist(a, b, c):
+        print("YES")
+    else:
+        print("NO")
