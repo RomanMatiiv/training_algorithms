e9577a504cf5addfe90c380bd654139c54fcbc49
matiiv <matiiv.r.a@gmail.com>
2021-06-13 16:58:39 +0300

    implement solution for problem d

--- lesson_1/D/solution_d.py
+++ lesson_1/D/solution_d.py
@@ -3,26 +3,43 @@ https://contest.yandex.ru/contest/27393/problems/D/
 """
 
 
-def get_solution(a: int, b: int, c: int):
-    x = None
+class NoSolution(Exception):
+    pass
 
-    if a == 0 and b == c*c:
-        print("MANY SOLUTION")
-        return x
+
+class ManySolution(Exception):
+    pass
+
+
+def get_solution(a: int, b: int, c: int) -> int:
+
+    if c < 0:
+        raise NoSolution
+
+    elif a == 0 and b == c*c:
+        raise ManySolution
 
     elif a == 0:
-        print("NO SOLUTION")
-        return x
+        raise NoSolution
 
     x = (c*c - b)/a
 
     if int(x) == x:
-        return x
+        return int(x)
     else:
-        print("NO SOLUTION")
-        return None
-
+        raise NoSolution
 
 
 if __name__ == "__main__":
-    pass
+    a = int(input())
+    b = int(input())
+    c = int(input())
+
+    try:
+        x = get_solution(a=a, b=b, c=c)
+    except NoSolution:
+        print("NO SOLUTION")
+    except ManySolution:
+        print("MANY SOLUTIONS")
+    else:
+        print(x)
